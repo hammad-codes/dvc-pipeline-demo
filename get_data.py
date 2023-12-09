9b15b10d41e62dd9f487a0656d642474c9d3bfc0
Hammad Habib <hammadhabib80@gmail.com>
2023-12-09 12:29:26 +0500

    1st commit -> accuracy=0.8666

---
+++ get_data.py
@@ -0,0 +1,10 @@
+
+# data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
+
+# Download the zipped dataset
+url = 'https://drive.google.com/file/d/1NFjX2SUFheYRACqiwC6Vno7Fh-mmYna5/view?usp=sharing'
+name = "data_raw.csv"
+
+import gdown
+
+gdown.download(url, name, quiet=False, fuzzy=True)
